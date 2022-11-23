892fceaf98be698fda202ce497a5942cf3259c8f
vv-p <vvp@protonmail.ch>
2022-11-23 22:29:16 +0300

    Add getting access_token, add test call to Evernote api

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python2.7
+# encoding: utf-8
 
 import sys
 import time
 import threading
+
+import click
+import yaml
 
 from urlparse import parse_qs, urlparse
 from evernote.api.client import EvernoteClient
@@ -25,14 +29,12 @@ class OauthRequestHandler(SimpleHTTPRequestHandler):
         global OAUTH_VERIFIER
 
         query = parse_qs(urlparse(self.path).query)
-        for key in query.keys():
-            print query[key]
+        for key, value in query.iteritems():
+            # Попытка просто обратиться к query приводит к падению с KeyError
             if key == "oauth_token":
-                print "oauth_token found"
-            print query["oauth_token"]
-
-        OAUTH_TOKEN = query["oauth_token"][0]
-        OAUTH_VERIFIER = query["oauth_verifier"][0]
+                OAUTH_TOKEN = value
+            if key == "oauth_verifier":
+                OAUTH_VERIFIER = value
 
         self.send_response(200)
         self.send_header("Content-Type", "text/html")
@@ -46,7 +48,7 @@ def http_server():
     httpd.serve_forever()
 
 
-def main():
+def get_access_token():
     global OAUTH_TOKEN
     global OAUTH_VERIFIER
 
@@ -55,27 +57,56 @@ def main():
     )
     hs.daemon = True
     hs.start()
-    #
-    # client = EvernoteClient(
-    #     consumer_key=CONSUMER_KEY,
-    #     consumer_secret=CONSUMER_SECRET,
-    #     sandbox=True  # Default: True
-    # )
-    # request_token = client.get_request_token(CALLBACK_URL)
-    # print request_token
-    #
-    # print client.get_authorize_url(request_token)
+
+    client = EvernoteClient(
+        consumer_key=CONSUMER_KEY,
+        consumer_secret=CONSUMER_SECRET,
+        sandbox=True  # Default: True
+    )
+    request_token = client.get_request_token(CALLBACK_URL)
+    print request_token
+
+    print client.get_authorize_url(request_token)
 
     while not OAUTH_TOKEN:
         # print "Waiting for oauth_token"
         time.sleep(1)
 
-    # access_token = client.get_access_token(
-    #     request_token['oauth_token'],
-    #     request_token['oauth_token_secret'],
-    #     OAUTH_VERIFIER,
-    # )
-    # print access_token
+    access_token = client.get_access_token(
+        request_token['oauth_token'],
+        request_token['oauth_token_secret'],
+        OAUTH_VERIFIER,
+    )
+    print access_token
+
+
+def main():
+    try:
+        with open('secrets.yaml', 'r') as f_secrets:
+            secrets = yaml.safe_load(f_secrets)
+    except IOError:
+        click.echo(
+            click.style("Can't load secrets from secrets.yaml file", fg="red")
+        )
+        return
+    except yaml.YAMLError:
+        click.echo(
+            click.style("secrets.yaml file corrupted", fg="red")
+        )
+        return
+
+    access_token = secrets.get("access_token")
+    if not access_token:
+        click.echo(
+            click.style("access_token in empty or not exists", fg="red")
+        )
+
+    client = EvernoteClient(token=access_token)
+    note_store = client.get_note_store()
+    notebooks = note_store.listNotebooks()
+
+    # Обкачивать аккаунт надо через SyncChunk
+
 
 
 if __name__ == '__main__':
